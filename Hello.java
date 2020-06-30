35f040292499e163ce0ecc5cfba891d76d09904a
kumam8 <manish.kumar@in.pega.com>
2020-06-30 15:28:03 +0530

    next commit again

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-         System.out.println("Git started..");
+         System.out.println("Git started..next changes");
         for (int i =0 ;i < 10; i++) {
             System.out.println("Hello World");
         }
